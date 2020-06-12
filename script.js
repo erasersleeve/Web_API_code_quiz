760c31d387d28cc05d1f4d293b148ccbff2d65eb
twtw <61638296+erasersleeve@users.noreply.github.com>
2020-06-11 20:02:02 -0400

    Everything functions except for the answer validation, something isn't working right. The first answer does, but the others seem to be off the iteration or something. Completed basic styling.

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ var cardH1 = document.createElement("div");
 var cardBody = document.createElement("div");
 var cardFooter = document.createElement("div");
 var time = "";
+
+header.className+= "navbar navbar-light bg-light";
+cardH1.className+="h1";
 //iterator for arrays
 i = 0;
 // Append all of our elements
@@ -15,13 +18,16 @@ cardEl.appendChild(cardH1);
 cardEl.appendChild(cardBody);
 cardEl.appendChild(cardFooter); 
 
-var questions = ["First Question", "Second Question", "Third Question", "Fourth Question"];
+var questions = ["Inside which HTML element do we put the Javascirpt?", 
+"What is the correct JavaScript syntax to change the content of the HTML element below?\n <p id='demo'>This is a demonstration.</p>", 
+"Where is the correct place to insert a JavaScript?", 
+"What is the correct syntax for referring to an external script called 'xxx.js'?"];
 
 var answers = [
-    { a: "1", b: "2", c: "3", d: "4"},
-    { a: "11", b: "22", c: "33", d: "44"},
-    { a: "111",  b: "222", c: "333", d: "444"},
-    { a: "1111", b: "2222", c: "3333", d: "4444"},
+    { a: "<scripting>", b: "<js>", c: "<script>", d: "javascript"},
+    { a: "document.getElementByName('p').innerHTML = 'Hello World!'", b: "document.getElementById('demo').innerHTML = 'Hello World!'", c: "#demo.innerHtml = 'Hello World!", d: "document.getElementByElement('p')innterHTML = 'Hello World'"},
+    { a: "The <body> section",  b: "The <head> section", c: "the <tail> section", d: "Both <head> and <body> sections"},
+    { a: "<script name = xxx.js>", b: "<script src = xxx.js>", c: "<script java = xxx.js>", d: "<coffee.java = xxx.js>"},
 ];
 
 var leader = [];
@@ -30,16 +36,21 @@ var leader = [];
 
 
 function mainPage () {
+    
     var scoreslink = document.createElement("button"); scoreslink.textContent="Highscores";scoreslink.addEventListener("click", function () {highScorespage()});
+    scoreslink.className+= "highscorebtn btn btn-outline-dark mr-auto";
     header.appendChild(scoreslink); 
     var startButton = document.createElement("button");
     startButton.textContent = "Get Started!";
+    startButton.className = "startButton btn btn-outline-dark";
     cardFooter.appendChild(startButton);
     startButton.addEventListener("click", function(event) {
         console.log(event);  
         cardFooter.removeChild(startButton);
         header.removeChild(scoreslink);
+        clock();
         quizpage ();
+        
     })
     
     cardH1.textContent = "Code Quiz!";
@@ -48,7 +59,9 @@ function mainPage () {
 }; 
 function quizpage () {
     i = 0;
-    clock();
+    
+    questions;
+    answers;
      //erase the message from the main page and replace the cardbody content with buttons
     cardBody.textContent = "";
     var buttongroup = document.createElement("div");
@@ -57,6 +70,11 @@ function quizpage () {
     var ans2 = document.createElement("button");
     var ans3 = document.createElement("button");
     var ans4 = document.createElement("button");
+    ans1.className+="ans btn btn-outline-secondary";
+    ans2.className+="ans btn btn-outline-secondary";
+    ans3.className+="ans btn btn-outline-secondary";
+    ans4.className+="ans btn btn-outline-secondary";
+
     buttongroup.appendChild(ans1);
     buttongroup.appendChild(ans2);
     buttongroup.appendChild(ans3);
@@ -71,14 +89,7 @@ function quizpage () {
     };
     answercontent();
     //not worrying about random yet
-    //group these buttons together
-    
-    
-    
-    //the first answer is always right, but we assign the answers at random to the buttons.
-    //if the button with the first answer is clicked only the next question is presented, if the others are 20 seconds are deducted
     //when i = questions.length the scores page shows, stopping the timer
-        
     //click event for our answer buttons
     
 
@@ -86,15 +97,15 @@ function quizpage () {
         if (i == 0) {
         
             ans1.addEventListener("click",answerSelectinvalid);
-            ans2.addEventListener("click",answerSelectvalid);
-            ans3.addEventListener("click",answerSelectinvalid);
+            ans2.addEventListener("click",answerSelectinvalid);
+            ans3.addEventListener("click",answerSelectvalid);
             ans4.addEventListener("click",answerSelectinvalid);
         };
         if (i == 1) {
             ans1.addEventListener("click",answerSelectinvalid);
-            ans2.addEventListener("click",answerSelectinvalid);
+            ans2.addEventListener("click",answerSelectvalid);
             ans3.addEventListener("click",answerSelectinvalid);
-            ans4.addEventListener("click",answerSelectvalid);
+            ans4.addEventListener("click",answerSelectinvalid);
         };
         if (i == 2) {
             ans1.addEventListener("click",answerSelectvalid);
@@ -108,36 +119,33 @@ function quizpage () {
             ans3.addEventListener("click",answerSelectvalid);
             ans4.addEventListener("click",answerSelectinvalid);
         };
-    function answerSelectvalid (event) {
-        if (i<(questions.length-1)) {
+    function answerSelectvalid () {
+        if (i<(questions.length)) {
             i++;
             answercontent();
+            console.log(questions.length);
+            console.log(i);
         }
-        
     };    
-    function answerSelectinvalid (event) {
+    function answerSelectinvalid () {
         
-        if (i<(questions.length-1)) {  
+        if (i<(questions.length)) {  
             i++;
             time-=20;
             answercontent();
-        }
-        else{
-        // once the last question is answered the buttons disappear and the score page opens
-        cardBody.removeChild(buttongroup);
-         
-    };
+            console.log(i);
+        }  
 
 
 };
     
 
     
-    buttongroup.setAttribute("style", "background-color:#4CAF50; border:1px solid green; color: white; padding 10px 24px; cursor: pointer; width:100%; display: block;");
-    ans1.setAttribute("style", "background-color:#4CAF50; border:1px solid green; color: white; padding 10px 24px; cursor: pointer; width: 100%; display: block;");
-    ans2.setAttribute("style", "background-color:#4CAF50; border:1px solid green; color: white; padding 10px 24px; cursor: pointer; width: 100%; display: block;");
-    ans3.setAttribute("style", "background-color:#4CAF50; border:1px solid green; color: white; padding 10px 24px; cursor: pointer; width: 100%; display: block;");
-    ans4.setAttribute("style", "background-color:#4CAF50; border:1px solid green; color: white; padding 10px 24px; cursor: pointer; width: 100%; display: block;");
+    // buttongroup.setAttribute("style", "background-color:#4CAF50; border:1px solid green; color: white; padding 10px 24px; cursor: pointer; width:100%; display: block;");
+    // ans1.setAttribute("style", "background-color:#4CAF50; border:1px solid green; color: white; padding 10px 24px; cursor: pointer; width: 100%; display: block;");
+    // ans2.setAttribute("style", "background-color:#4CAF50; border:1px solid green; color: white; padding 10px 24px; cursor: pointer; width: 100%; display: block;");
+    // ans3.setAttribute("style", "background-color:#4CAF50; border:1px solid green; color: white; padding 10px 24px; cursor: pointer; width: 100%; display: block;");
+    // ans4.setAttribute("style", "background-color:#4CAF50; border:1px solid green; color: white; padding 10px 24px; cursor: pointer; width: 100%; display: block;");
 
     // from how to make verticle buttons w3 .btn-group button:not(:last-child) {border-bottom: none;} apparently prevents double borders
     // * Add a background color on hover */.btn-group button:hover {background-color: #3e8e41;}
@@ -150,53 +158,12 @@ function scorePage () {
         cardH1.textContent = "Your Score";
         cardBody.textContent = time;
         var leaderinput = document.createElement("input");
-        leaderinput.textContent = "Put you name or initials here to submit a high score!"
+        leaderinput.placeholder = "Put you name or initials here to submit a high score!";
         cardBody.appendChild(leaderinput);
         var scorebtn = document.createElement("button");
         scorebtn.textContent = "Go to Leaderboards";
+        scorebtn.className+= "btn btn-outline-dark";
         cardBody.appendChild(scorebtn);
-        var leaderList = "#leaderList";
-        
-    
-    
-    // init();
-    // function renderLeaders() {
-    //     // Clear leaderList element and update leaderCountSpan
-    //     leaderList.innerHTML = "";
-    //     leaderCountSpan.textContent = leader.length;
-      
-    //     // Render a new li for each todo
-    //     for (var i = 0; i < leader.length; i++) {
-    //       var leader = leader[i];
-      
-    //       var li = document.createElement("li");
-    //       li.textContent = leader;
-    //       li.setAttribute("data-index", i);
-      
-    //       leaderList.appendChild(li);
-    //     }
-    //   }
-    // function init() {
-    //     // Write code here to check if there are todos in localStorage
-    //     // If so, parse the value from localStorage and assign it to the todos variable
-    //     var templeaders = localStorage.getItem("leader");
-        
-    //     templeaders = JSON.parse(templeaders);
-        
-    //     console.log(templeaders);
-        
-    //     leader = templeaders;
-        
-    //     // Render leader to the DOM
-    //     renderLeaders();
-    //     }
-    //     //stringify array and save to leader
-    // function storeLeaders () {
-    //     localStorage.setItem("leader", JSON.stringify(leader));
-    // };
-
-    // renderLeaders();
-    // storeLeaders();
 
 
 
@@ -255,6 +222,7 @@ function highScorespage () {
 function clock() {
     time = 80;
     var playerClock = document.createElement("div");
+    playerClock.className+="playerClock ml-auto";
     header.appendChild(playerClock);
     playerClock.setAttribute("style", "font-size:25px; text-align:center;");
     var timerinterval = setInterval(function() {
@@ -262,10 +230,11 @@ function clock() {
         // console.log(i);
         playerClock.textContent = Number(time);
        //stop timer when quiz (either time = 0 or all questions have been answered)
-       if (i == questions.length - 1 || time <= 0) {
+       if (i == questions.length || time <= 0) {
         clearInterval(timerinterval);   
+        scorePage ();
         header.removeChild(playerClock);
-        scorePage ();}
+        }
     }, 1000);
 };
 mainPage();
